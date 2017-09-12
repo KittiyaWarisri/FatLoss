0c1d5326a9e91bfa7261e3049a7e21c7b94418bc
Mindmie <dreamiing.allday@gmail.com>
2017-09-13 00:27:50 +0700

    update

---
+++ app/src/main/java/com/example/saiby/fatloss/InputInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.saiby.fatloss;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_info);
+    }
+}
